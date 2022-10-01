b6c973df5df5aee494d47887d934296ff977f3cf
Elfiato <doker912@gmail.com>
2022-10-01 14:20:54 +0500

    add homework1, requirements.txt

---
+++ homework1/base.py
@@ -0,0 +1,69 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, TimeoutException, ElementNotInteractableException, \
+    InvalidElementStateException
+from selenium.webdriver.common.action_chains import ActionChains
+import locators
+
+WAITING_TIME = 10
+
+
+class Base:
+    def __init__(self, driver, url):
+        self.driver = driver
+        self.url = url
+
+    def open(self):
+        self.driver.get(self.url)
+
+    def is_element_present(self, how, what, timeout=WAITING_TIME):
+        try:
+            WebDriverWait(self.driver, timeout).until(
+                EC.presence_of_element_located((how, what)))
+        except (NoSuchElementException, TimeoutException):
+            return False
+        return True
+
+    def get_present_element(self, how, what):
+        if self.is_element_present(how, what):
+            return self.driver.find_element(how, what)
+        else:
+            assert self.is_element_present(how, what), f'Элемент с локатором {how} :: {what} не найден'
+
+    def move_to_present_element(self, how, what):
+        action = ActionChains(self.driver)
+        element = self.get_present_element(how, what)
+        action.move_to_element(element).perform()
+
+    def is_element_clickable(self, how, what, timeout=WAITING_TIME):
+        try:
+            WebDriverWait(self.driver, timeout).until(EC.element_to_be_clickable((how, what)))
+        except (ElementNotInteractableException, InvalidElementStateException, TimeoutException):
+            return False
+        return True
+
+    def get_clickable_element(self, how, what):
+        if self.is_element_clickable(how, what):
+            return self.driver.find_element(how, what)
+        else:
+            assert self.is_element_present(how, what), f'На элемент с локатором {how} :: {what} невозможно нажать.'
+
+    def is_element_visible(self, how, what, timeout=WAITING_TIME):
+        try:
+            WebDriverWait(self.driver, timeout).until(EC.visibility_of_element_located((how, what)))
+        except (NoSuchElementException, TimeoutException):
+            return False
+        return True
+
+    def login_func(self, login, password):
+        login_button_in_header = self.get_present_element(*locators.LOGIN_BUTTON_IN_HEADER)
+        login_button_in_header.click()
+        self.is_element_present(*locators.AUTHORIZATION_FORM)
+        email_field = self.get_present_element(*locators.EMAIL_FIELD)
+        email_field.clear()
+        email_field.send_keys(login)
+        password_field = self.get_present_element(*locators.PASSWORD_FIELD)
+        password_field.clear()
+        password_field.send_keys(password)
+        login_button_in_authorization_form = self.get_present_element(*locators.LOGIN_BUTTON_IN_AUTHORIZATION_FORM)
+        login_button_in_authorization_form.click()
---
+++ homework1/test_homework1.py
@@ -0,0 +1,67 @@
+import pytest
+import locators
+from base import Base
+
+MAIN_PAGE = "https://target-sandbox.my.com/"
+LOGIN = 'jo.ry@mail.ru'
+PASSWORD = '12345A'
+NAME = 'Евгений'
+
+
+@pytest.mark.UI
+@pytest.mark.parametrize('login, password',
+                         [(LOGIN, PASSWORD),
+                          pytest.param('1' + LOGIN, PASSWORD,
+                                       marks=pytest.mark.xfail(reason='Invalid login, valid password')),
+                          pytest.param(LOGIN, PASSWORD + '1',
+                                       marks=pytest.mark.xfail(reason='Valid login, invalid password')),
+                          ])
+def test_login(driver, login, password):
+    page = Base(driver, MAIN_PAGE)
+    page.open()
+    page.login_func(login, password)
+    assert page.is_element_present(*locators.USER_NAME_BUTTON), 'Авторизация не произведена.'
+
+
+@pytest.mark.UI
+def test_logout(driver):
+    page = Base(driver, MAIN_PAGE)
+    page.open()
+    page.login_func(LOGIN, PASSWORD)
+    username_button = page.get_present_element(*locators.USER_NAME_BUTTON)
+    username_button.click()
+    page.move_to_present_element(*locators.SHOWN_RIGHT_MENU)
+    logout_button = page.get_present_element(*locators.LOGOUT_BUTTON)
+    logout_button.click()
+    assert page.is_element_present(*locators.LOGIN_BUTTON_IN_HEADER), 'Выход из системы не произведен.'
+
+
+@pytest.mark.UI
+def test_change_profile_information(driver):
+    page = Base(driver, MAIN_PAGE)
+    page.open()
+    page.login_func(LOGIN, PASSWORD)
+    profile_button = page.get_clickable_element(*locators.PROFILE_BUTTON)
+    profile_button.click()
+    name_field = page.get_clickable_element(*locators.NAME_FIELD)
+    name_field.clear()
+    name_field.send_keys(NAME)
+    save_button = page.get_present_element(*locators.SAVE_BUTTON)
+    save_button.click()
+    assert page.is_element_visible(
+        *locators.SUCCESS_NOTIFICATION), 'Оповещение об успешном сохранении данных профиля не появилось ' \
+                                         'после нажатия кнопки "Сохранить".'
+
+
+@pytest.mark.UI
+@pytest.mark.parametrize('locator, label, label_text',
+                         [(locators.PROFILE_BUTTON, locators.PROFILE_CATEGORY_LABEL, 'Контактная информация'),
+                          (locators.BILL_BUTTON, locators.BILL_CATEGORY_LABEL, 'ПЛАТЕЛЬЩИК')])
+def test_go_to_center_module_categories(driver, locator, label, label_text):
+    page = Base(driver, MAIN_PAGE)
+    page.open()
+    page.login_func(LOGIN, PASSWORD)
+    categories_button = page.get_clickable_element(*locator)
+    categories_button.click()
+    text = page.get_present_element(*label).text
+    assert text == label_text, f'Переход в категорию с локатором {locator} не произведен.'
